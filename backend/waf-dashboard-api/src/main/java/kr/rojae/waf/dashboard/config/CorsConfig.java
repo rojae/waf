5d8872f57b754762a651c30ba0e9411eaf1bdc8f
jaeseoh <jaeseoh@gmarket.com>
2025-09-01 03:56:30 +0900

    fix: delete cors localhost

--- backend/waf-dashboard-api/src/main/java/kr/rojae/waf/dashboard/config/CorsConfig.java
+++ backend/waf-dashboard-api/src/main/java/kr/rojae/waf/dashboard/config/CorsConfig.java
@@ -10,7 +10,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3001", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false)
